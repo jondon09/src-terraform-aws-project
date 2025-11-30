4afb1b884ffe2c7e67540ed6bb1b2bba83a89341
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-11-29 19:29:00 -0500

    added architecture image to readme.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # src-terraform-aws-project
+# Terraform project to provision resources 
+
+<img src="/Terraform-on-aws.svg" >
--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.23.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "iamadmin-general"
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "images-bucket" {
   bucket = "terraform-test-bucket-20251228"
 
   tags = {
