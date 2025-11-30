1fdd0afba8f2ab00d29e9dff87a6a2fc0e247306
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-11-29 22:54:46 -0500

    refactoring terraform project

--- variables.tf
+++ variables.tf
@@ -1,5 +1,27 @@
 variable "aws_region" {
-  description: "The aws region to deploy in"
-  type: "string"
-  default: "us-east-1"
+  description = "The aws region to deploy in"
+  type = "string"
+  default = "us-east-1"
+}
+
+variable "aws_profile" {
+  description = "profile for Deploying resource on AWS"
+  type = "string"
+  default = "iamadmin-general"
+}
+
+
+variable "aws_s3_bucket_name"{
+  description = "The Unique name of the AWS Bucket"
+  type = "string"
+  default = "terraform-test-bucket-20251228"
+}
+
+variable "aws_tags"  {
+  description = "Tags to set on AWS objects"
+  type        =  map(string)
+  default = {
+    "Team" = "GCA-platform"
+    "Environment" = "dev"
+  }
 }
