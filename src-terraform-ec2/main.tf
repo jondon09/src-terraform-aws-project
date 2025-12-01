9e41b8939549ff8360a7ad79c532b03524b79bcd
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-12-01 07:55:10 -0500

    added bootstrap for dynamodb remote locking for ec2 project

--- src-terraform-ec2/main.tf
+++ src-terraform-ec2/main.tf
@@ -39,7 +39,7 @@ resource "aws_vpc_security_group_ingress_rule" "example" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "example" {
-  security_group_id = aws_security_group.vpc_security_group
+  security_group_id = aws_security_group.vpc_security_group.id
 
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "-1"
