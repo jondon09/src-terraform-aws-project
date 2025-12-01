08f5db072caf0862bc9fd041ba39e021d9c08c38
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-12-01 07:55:44 -0500

    added remote backend with state locking to main terraform project

--- provider.tf
+++ provider.tf
@@ -7,6 +7,19 @@ terraform {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-backend-31112025"
+    key            = "src-terraform-aws-project/terraform.tfstate"
+    region         = "us-east-1"
+    profile        = "iamadmin-general"
+    dynamodb_table = "terraform_state_lock"
+    encrypt        = "true"
+  }
+}
+
+
+
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
