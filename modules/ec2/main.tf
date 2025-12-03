96439031c7ccece8ab3dca82d15835d4670f5a91
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-12-03 10:14:59 -0500

    added terraform state files

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -12,28 +12,28 @@ resource "aws_instance" "instance" {
 locals {
   ubuntu = var.image_os == "ubuntu" ? data.aws_ami.ubuntu_server_latest.id : null
   amazon = var.image_os == "amazon linux" ? data.aws_ami.amazon_linux_latest.id : null
-  image = coalesce(local.ubuntu, local.amazon)
+  image  = coalesce(local.ubuntu, local.amazon)
 }
 
 # Retrieve latest Amazon Linux AMI
 # Data blocks used to read data from external source
 data "aws_ami" "amazon_linux_latest" {
   most_recent = true
-  owner = ["amazon"]
+  owner       = ["amazon"]
   filter {
-    name = "name"
+    name   = "name"
     values = ["*al2023-ami-2023*x86_64*"]
   }
   filter {
-    name = "platform-details"
+    name   = "platform-details"
     values = ["Linux/Unix"]
   }
   filter {
-    name = "root-device-type"
+    name   = "root-device-type"
     values = ["ebs"]
   }
   filter {
-    name = "architecture"
+    name   = "architecture"
     values = ["x86_64"]
   }
 }
