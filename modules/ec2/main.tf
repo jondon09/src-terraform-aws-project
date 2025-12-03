af1891157ed78cfe8d300be2ed332e910f4be2a9
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-12-03 10:05:52 -0500

    added terraform state files

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "instance" {
   }
   tags = var.tagging
 }
+
+# Store the AMI IDs that we retrieve
+
+locals {
+  ubuntu = var.image_os == "ubuntu" ? data.aws_ami.ubuntu_server_latest.id : null
+  amazon = var.image_os == "amazon linux" ? data.aws_ami.amazon_linux_latest.id : null
+  image = coalesce(local.ubuntu, local.amazon)
+}
+
